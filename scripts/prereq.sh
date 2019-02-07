f8518b73cf06d59b74b3580dfcd114be9541908e
rwrigg-hass <47410131+rwrigg-hass@users.noreply.github.com>
2019-02-07 00:00:58 -0600

    Update prereq.sh
    Support for RHEL7 and misc improvements
    Added SUDO_USER validation, as this is used throughout the script

--- scripts/prereq.sh
+++ scripts/prereq.sh
@@ -1,25 +1,49 @@
 #!/bin/bash
-VERSION=`cat /etc/lsb-release | grep RELEASE | cut -d"=" -f2`
-if [ $VERSION = "18.04" ]; then
-  DOCKER="docker-ce"
-else
-  DOCKER="docker-ce"
+
+if [[ -z $SUDO_USER || $SUDO_USER == "root" ]] ;then
+  echo "Must sudo as desired Elastic_Stack service user" 
+  exit
 fi
-apt update
-if [ $(dpkg-query -W -f='${Status}' git 2>/dev/null | grep -c "ok installed") -eq 0 ];
-then
-  echo "Installing git"
-  apt install -y git
+
+DIST=`lsb_release -si`
+VERSION=`lsb_release -sr`
+
+pkginstall () {
+  if [ $DIST = "Ubuntu" ] ;then
+    if [ $(dpkg-query -W -f='${Status}' $1 2>/dev/null | grep -c "ok installed") -eq 0 ] ;then
+        echo "Installing $1"
+        apt install -y $1
+      else
+        echo "$1 is already installed"	  
+	fi
+  elif [ $DIST = "RedHatEnterpriseServer" ] ;then
+    yum install -y $1
+  fi
+}
+
+if [ $DIST = "Ubuntu" ] ;then
+  PKGMGR="apt"
+  if [ $VERSION = "18.04" ]; then
+    DOCKER="docker-ce"
+  else
+    DOCKER="docker-ce"
+  fi
+elif [ $DIST = "RedHatEnterpriseServer" ] ;then
+  if [[ $VERSION =~ ^7\. ]] ;then
+    PKGMGR="yum"
+    DOCKER="docker-ce"
+  else
+    echo "Unsupport Red Hat Enterprise Linux Release"
+	exit
+  fi
 else
-  echo "Git is already installed"
+  echo "Unsupported Distribution"
+  exit
 fi
-if [ $(dpkg-query -W -f='${Status}' curl 2>/dev/null | grep -c "ok installed") -eq 0 ];
-then
-  echo "Installing curl"
-  apt install -y curl
-else
-  echo "Curl is already installed"
-fi
+
+${PKGMGR} update
+pkginstall git
+pkginstall curl
 
 if [ ! -d /opt/elastic_stack ];
 then
@@ -28,33 +52,45 @@ then
   git clone https://github.com/HASecuritySolutions/elastic_stack.git
   chown -R ${SUDO_USER} /opt/elastic_stack
 fi
-if grep -q 'deb \[arch=amd64\] https://download.docker.com/linux/ubuntu' /etc/apt/sources.list
-then
-  echo "Docker software repository is already installed"
-else
-  echo "Docker software repository is not installed. Installing"
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-  sudo apt-get update
+
+if [ $DIST = "Ubuntu" ] ;then
   if grep -q 'deb \[arch=amd64\] https://download.docker.com/linux/ubuntu' /etc/apt/sources.list
   then
-    echo "Docker software repository is now installed"
+    echo "Docker software repository is already installed"
+  else
+    echo "Docker software repository is not installed. Installing"
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo apt-get update
+    if grep -q 'deb \[arch=amd64\] https://download.docker.com/linux/ubuntu' /etc/apt/sources.list
+    then
+      echo "Docker software repository is now installed"
+    fi
   fi
+elif [ $DIST = "RedHatEnterpriseServer" ] ;then
+  pkginstall yum-utils
+  yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 fi
-if [ $(dpkg-query -W -f='${Status}' docker 2>/dev/null | grep -c "ok installed") -eq 0 ];
-then
-  echo "Installing docker"
-  sudo apt-get install -y $DOCKER
-else
-  echo "Docker is already installed"
+
+if [ $DIST = "RedHatEnterpriseServer" ] ;then
+  yum erase -y docker docker-common docker-io
 fi
+
+pkginstall $DOCKER
+
+if [ $DIST = "RedHatEnterpriseServer" ] ;then
+  systemctl enable docker
+  systemctl start docker
+fi
+
 if grep docker /etc/group | grep -q ${SUDO_USER}
 then
-  echo "Current user already member of docker group"
+  echo "${SUDO_USER} user already member of docker group"
 else
-  echo "Adding current user to docker group"
+  echo "Adding ${SUDO_USER} user to docker group"
   sudo usermod -aG docker ${SUDO_USER}
 fi
+
 if [ -f /usr/local/bin/docker-compose ];
 then
   echo "Docker Compose is already installed"
@@ -63,12 +99,18 @@ else
   sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
 fi
-if grep -q 'vm.max_map_count' /etc/sysctl.conf
-then
-  echo "VM Max Map Count already configured"
-else
+
+if [ $DIST = "Ubuntu" ] ;then
+  if grep -q 'vm.max_map_count' /etc/sysctl.conf
+  then
+    echo "VM Max Map Count already configured"
+  else
+    echo "Setting vm.max_map_count to 262144"
+    sudo sysctl -w vm.max_map_count=262144
+    echo "vm.max_map_count=262144" | sudo tee -a /etc/sysctl.conf
+  fi
+elif [ $DIST = "RedHatEnterpriseServer" ] ;then
   echo "Setting vm.max_map_count to 262144"
-  sudo sysctl -w vm.max_map_count=262144
-  echo "vm.max_map_count=262144" | sudo tee -a /etc/sysctl.conf
+  echo "vm.max_map_count=262144" > /etc/sysctl.d/99-elastic_stack.conf
+  sysctl --load /etc/sysctl.d/99-elastic_stack.conf
 fi
-chown -R ${SUDO_USER}: /opt/elastic_stack
